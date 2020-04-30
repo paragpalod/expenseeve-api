9c27bb213f581a3b2a30b64dddb11c9fc8a2630e
paragpalod <phpalod@gmail.com>
2020-04-30 23:32:22 +0530

    10. Created Config.js file with secret

--- server/config/index.js
+++ server/config/index.js
@@ -1,21 +1,5 @@
 const configuration = {
-  serverSecret: process.env.API_SECRET, // change this secret to a 32bit string for jwt token secret
-  services: {
-    email: {
-      fromEmail: process.env.EMAIL_ID, // add your mail id here
-      from: `"Team Hapi Project" ${process.env.EMAIL_ID}`, // this will give your every email a displayname
-      secret: process.env.EMAIL_SECRET// secrete key provided by your email service provider
-    }
-  },
-  verificationLink: {
-    baseURL: `http://localhost:3000/auth/verifyUser?token=`
-  },
-  resetPasswordLink: {
-    baseURL: `http://localhost:3000/auth/resetPassword?token=`
-  }
+  serverSecret: 'expenseeve-api-secret'
 };
 
 exports.serverSecret = configuration.serverSecret;
-exports.email = configuration.services.email;
-exports.verificationLink = configuration.verificationLink;
-exports.resetPasswordLink = configuration.resetPasswordLink;
