fbcd60b418b69233b26732be7b2d256f2dae2c52
paragpalod <phpalod@gmail.com>
2020-05-02 17:32:17 +0530

    21. created expense Schema

---
+++ server/models/core/expense.js
@@ -0,0 +1,26 @@
+module.exports = function (Schema) {
+  const Expense = new Schema({
+    itemName: {
+      type: String,
+      required: true
+    },
+    amount: {
+      type: Number,
+      required: true
+    },
+    userID: {
+      type: Schema.Types.ObjectId,
+      required: true
+    },
+    categoryID: {
+      type: Schema.Types.ObjectId,
+      required: true
+    },
+    deletedAt: {
+      type: Number,
+      default: null
+    }
+  }, { timestamps: true });
+
+  return Expense;
+};
